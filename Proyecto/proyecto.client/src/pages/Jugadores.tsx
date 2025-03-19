fe6b54202d96e1177accd7de77da44d9b4c64492
Juan Marroquin <juanitomarroquinreyes38@gmail.com>
2025-03-18 18:07:58 -0600

    Versión 1.0
    Corregido:
    - Opciones del compilador en tsconfig.json
    - Los estilos de index.css y App.css
    Agregado:
    - Estructura de carpetas en src para un mejor manejo de los componentes y servicios a llamar en el front-end
    - Archivo AppRoutes.tsx para manejar las rutas de la barra de navegacion y las rutas accesibles para los usuarios
    - Agregada barra de navegación y sus respectivos estilos, además de la navegación básica entre las secciones: Inicio, partidos, torneos, jugadores, equipos y login

---
+++ Proyecto/proyecto.client/src/pages/Jugadores.tsx
@@ -0,0 +1,10 @@
+const Jugadores = () => {
+    return (
+        <div>
+            <h1>Jugadores</h1>
+            <p>Listado y gestión de Jugadores.</p>
+        </div>
+    );
+};
+
+export default Jugadores;
