9f478c4b65d25c8db89f5ae1412198f80cd204d1
ErickBV18 <161788333+ErickBV18@users.noreply.github.com>
2025-03-20 13:47:45 -0600

    Creacion_Modelos_1
    Se creo la carpeta Models con los modelos de Usuario y Torneo.

---
+++ Proyecto/Proyecto.Server/Models/Torneo.cs
@@ -0,0 +1,16 @@
+﻿namespace Proyecto.Server.Models
+{
+    public class Torneo
+    {
+        public int TorneoId { get; set; }
+        public string Nombre { get; set; } = null!;
+        public DateOnly FechaInicio { get; set; }
+        public DateOnly FechaFin { get; set; }
+        public string Descripcion { get; set; } = null!;
+        public string BasesTorneo { get; set; } = null!;
+        public DateOnly FechaInicioInscripcion { get; set; }
+        public DateOnly FechaFinInscripcion { get; set; }
+        public int CantidadParticipantes { get; set; }
+        public int UsuarioId { get; set; }
+    }
+}
---
+++ Proyecto/Proyecto.Server/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace Proyecto.Server.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Contrasenia { get; set; } = null!;
+        public string TipoRol { get; set; } = null!;
+        public string Estado { get; set; } = null!;
+        public string CorreoElectronico { get; set; } = null!;
+    }
+}
