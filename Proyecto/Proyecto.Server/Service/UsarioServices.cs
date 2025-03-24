38a987ab0c1feff873bc367c63b88d7582c473d7
ErickBV18 <161788333+ErickBV18@users.noreply.github.com>
2025-03-24 13:02:55 -0600

    Creación del servicio Usuario
    se creo el servicio donde se registrara un usuario y el poder ingresar a su cuenta.

--- Proyecto/Proyecto.Server/Service/UsarioServices.cs
+++ Proyecto/Proyecto.Server/Service/UsarioServices.cs
@@ -1,6 +1,56 @@
-﻿namespace Proyecto.Server.Service
+﻿
+using Proyecto.Server.Models;
+using System;
+using System.Security.AccessControl;
+using Proyecto.Server.DAL;
+using Proyecto.Server.DTOs;
+using System.Diagnostics;
+using BCrypt.Net;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+
+
+namespace Proyecto.Server.Service
+
 {
-    public class UsarioServices
+    public class UsuarioServices
     {
+        private readonly StoreProcedure _storeProcedure;
+
+
+        public UsuarioServices(StoreProcedure storeProcedure)
+        {
+            _storeProcedure = storeProcedure;
+        }
+
+        public void RegistrarUsuario(string Nombre, string Apellido, string Contrasenia, string TipoRol, string CorreoElectronico)
+        {
+            string hash = BCrypt.Net.BCrypt.HashPassword(Contrasenia);
+
+            var Usuario = new RegistroDTO();
+
+            Usuario.Nombre = Nombre;
+            Usuario.Apellido = Apellido;
+            Usuario.Contrasenia = hash;
+            Usuario.TipoRol = TipoRol;
+            Usuario.CorreoElectronico = CorreoElectronico;
+
+            var ParametrosUsuario = new Dictionary<string, object>()
+            {
+                {"@nombre", Usuario.Nombre},
+                {"@apellido", Usuario.Apellido},
+                {"@password", Usuario.Contrasenia },
+                {"@rol", Usuario.TipoRol },
+                {"@correo", Usuario.CorreoElectronico },
+            };
+
+            _storeProcedure.EjecutarProcedimientoAlmacenado("sp_createUser", System.Data.CommandType.StoredProcedure, ParametrosUsuario, null);
+        }
+
+        public bool IncioUsuario(string PasswordHash, string Password)
+        {
+            bool PasswordValida = BCrypt.Net.BCrypt.Verify(Password, PasswordHash);
+
+            return PasswordValida;
+        }
     }
 }
