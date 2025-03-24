105a530dbb996a96d90ca5e933db1ac36df7b351
ErickBV18 <161788333+ErickBV18@users.noreply.github.com>
2025-03-24 11:36:37 -0600

    Creacion del DTO RegistroDTO
    Se creo el primer DTO para el registro del usuario

---
+++ Proyecto/Proyecto.Server/DTOs/RegistroDTO.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.OpenApi.Interfaces;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Proyecto.Server.DTOs
+{
+    public class RegistroDTO
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Contrasenia { get; set; }
+        public string TipoRol {  get; set; }
+        public string CorreoElectronico { get; set; }
+    }
+}
