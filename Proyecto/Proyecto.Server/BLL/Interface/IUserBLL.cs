2eafe92cbe006040bcbc78c825778233e831a7bd
Kenett David Maldonado Jocol <kenettjocol@umes.edu.gt>
2025-03-25 11:29:03 -0600

    Cambio de lógica de capas
    Se agrego una nueva estructura a nuestro proyecto en N capas

---
+++ Proyecto/Proyecto.Server/BLL/Interface/IUserBLL.cs
@@ -0,0 +1,9 @@
+﻿using Proyecto.Server.DTOs;
+
+namespace Proyecto.Server.BLL.Interface
+{
+    public interface IUserBLL
+    {
+        public bool AutenticarUsuario(RegistroDTO.objAuthParameter parametrosPeticion);
+    }
+}
---
+++ Proyecto/Proyecto.Server/BLL/Repository/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
+using Proyecto.Server.BLL.Interface;
+using Proyecto.Server.DAL;
+
+namespace Proyecto.Server.BLL.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly StoreProcedure _storeProcedure;
+        public UserRepository(StoreProcedure storeProcedure)
+        {
+            _storeProcedure = storeProcedure;
+        }
+
+        public DataTable ObtenerCredenciales(string correo)
+        {
+            try
+            {
+                var parametrosEntrada = new Dictionary<string, object>
+                {   
+                    {"@correo",correo}
+                };
+
+                return (DataTable)_storeProcedure.EjecutarProcedimientoAlmacenado("sp_getCredencialesUsuario", CommandType.StoredProcedure, parametrosEntrada, null);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error, no se encontro las credenciales del usuario" + ex.Message);
+            }
+
+
+        }
+
+
+
+
+    }
+}
